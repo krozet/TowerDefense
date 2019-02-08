a80690e408be9c48018428d50f9f08db49ab6a15
krozet <keawa.rozet@yahoo.com>
2019-02-07 20:27:37 -0800

    added waypoints and an enemy that follows the waypoints from start to end.

---
+++ TowerDefense/Assets/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 40f;
+
+    private Transform targetWavePoint;
+    private int wavePointIndex = 0;
+
+    void Start() {
+        targetWavePoint = WayPoints.wayPoints[0];
+    }
+
+    private void Update() {
+        Vector3 dir = targetWavePoint.position - this.transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(this.transform.position, targetWavePoint.position) <= 0.4f) {
+            GetNextWayPoint();
+        }
+    }
+
+    private void GetNextWayPoint() {
+
+        if (wavePointIndex >= WayPoints.wayPoints.Length - 1) {
+            Destroy(gameObject);
+            return;
+        } 
+        wavePointIndex++;
+        targetWavePoint = WayPoints.wayPoints[wavePointIndex];
+    }
+}
---
+++ TowerDefense/Assets/WayPoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour {
+    public static Transform[] wayPoints;
+
+    void Awake() {
+        initWayPoints();
+    }
+
+    private void initWayPoints() {
+        wayPoints = new Transform[transform.childCount];
+
+        for (int i = 0; i < wayPoints.Length; i++) {
+            wayPoints[i] = transform.GetChild(i);
+        }
+    }
+}
