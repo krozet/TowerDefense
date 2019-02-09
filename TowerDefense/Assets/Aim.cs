2e1207f1a2328150f447ba03230f2cbc9df77670
krozet <keawa.rozet@yahoo.com>
2019-02-08 19:23:50 -0800

    towers now aim and rotate toward enemies.

---
+++ TowerDefense/Assets/Aim.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+
+    private Transform target;
+    public float range = 15f;
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    private void Update() {
+        if (target == null) {
+            return;
+        }
+
+        AimAtEnemy();
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies) {
+            float distanceToEnemy = Vector3.Distance(this.transform.position, enemy.transform.position);
+
+            if (distanceToEnemy < shortestDistance) {
+                nearestEnemy = enemy;
+                shortestDistance = distanceToEnemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range) {
+            target = nearestEnemy.transform;
+        } else {
+            target = null;
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, range);
+    }
+
+    void AimAtEnemy() {
+        float damping = 10f;
+        Vector3 dir = target.position - this.transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * damping).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+}
