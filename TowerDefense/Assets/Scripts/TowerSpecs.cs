41806620eabaf1abd041a904dd403abebddc3187
krozet <keawa.rozet@yahoo.com>
2019-02-11 14:09:28 -0800

    TowerSpecs updated and nullrefence is not an issue.

--- TowerDefense/Assets/Scripts/TowerSpecs.cs
+++ TowerDefense/Assets/Scripts/TowerSpecs.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Aim : MonoBehaviour
+public class TowerSpecs : MonoBehaviour
 {
 
     private Transform target;
